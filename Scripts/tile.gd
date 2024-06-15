2522309c6c350acbffa1a6b52b8c37b64864b844
Annikatt <anneke256@gmail.com>
2024-06-15 15:39:54 -0400

    hover stuff, woo!

--- Scripts/tile.gd
+++ Scripts/tile.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var _farmType: Global.FarmType = Global.FarmType.Empty
 @onready var tileSprite: AnimatedSprite2D = get_node("TileSprite")
+@onready var hoverSprite: AnimatedSprite2D = get_node("HoverSprite")
+var hovered = false
 
 @export var farmType: Global.FarmType:
 	get:
@@ -16,9 +18,25 @@ func updateTileImage():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tileSprite.play()
+	hoverSprite.hide()
 	updateTileImage()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if hovered:
+		if Input.is_action_just_released('RightClick'):
+			print("right click" + str(_farmType))
+		elif Input.is_action_just_released('LeftClick'):
+			print("left click" + str(_farmType))
+
+
+func _on_mouse_entered():
+	hovered = true
+	hoverSprite.show()
+	hoverSprite.play()
+
+
+func _on_mouse_exited():
+	hovered = false
+	hoverSprite.hide()
