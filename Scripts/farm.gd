b576199b2c09beadaa0fa962b1b022c2d879f223
Annikatt <anneke256@gmail.com>
2024-06-15 16:17:18 -0400

    lotsa crap

--- Scripts/farm.gd
+++ Scripts/farm.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+
+@onready var tileOptions: ItemList = $TileOptions
+@onready var farmTiles = $FarmTiles
+var activeTile = null
+
 var day = 1
 
 func new_day():
@@ -7,17 +12,40 @@ func new_day():
 	%DayCounter.text = "Day " + str(day)
 
 func testTiles():
-	var A1 = get_node("TileA1")
+	var A1 = get_node("FarmTiles/TileA1")
 	A1.farmType = Global.FarmType.Empty
-	var A2 = get_node("TileA2")
+	var A2 = get_node("FarmTiles/TileA2")
 	A2.farmType = Global.FarmType.Wheat
-	var A3 = get_node("TileA3")
+	var A3 = get_node("FarmTiles/TileA3")
 	A3.farmType = Global.FarmType.Shrine
-	var A4 = get_node("TileA4")
+	var A4 = get_node("FarmTiles/TileA4")
 	A4.farmType = Global.FarmType.Vegetable
-	var A5 = get_node("TileA5")
+	var A5 = get_node("FarmTiles/TileA5")
 	A5.farmType = Global.FarmType.Pasture
 
 func _ready():
 	#pass
 	testTiles()
+	tileOptions.hide()
+	for tile in $FarmTiles.get_children():
+		tile.right_click.connect(_right_clicked.bind(tile))
+		tile.left_click.connect(_left_clicked.bind(tile))
+
+func _right_clicked(tile):
+	activeTile = tile
+	tileOptions.position = tile.position + Vector2(30,30)
+	tileOptions.show()
+
+func _left_clicked(tile):
+	print("lc")
+
+
+func _on_tile_options_item_clicked(index, at_position, mouse_button_index):
+	if index == 5:
+		activeTile = null
+		tileOptions.hide()
+	else:
+		activeTile.set_farmtype(index)
+		activeTile = null
+		tileOptions.hide()
+		new_day()
--- Scripts/tile.gd
+++ Scripts/tile.gd
@@ -3,7 +3,10 @@ extends Node2D
 var _farmType: Global.FarmType = Global.FarmType.Empty
 @onready var tileSprite: AnimatedSprite2D = get_node("TileSprite")
 @onready var hoverSprite: AnimatedSprite2D = get_node("HoverSprite")
+@onready var selectedSprite: AnimatedSprite2D = get_node("SelectedSprite")
 var hovered = false
+signal right_click
+signal left_click
 
 @export var farmType: Global.FarmType:
 	get:
@@ -12,6 +15,10 @@ var hovered = false
 		_farmType = value
 		updateTileImage()
 
+func set_farmtype(value):
+	_farmType = value
+	updateTileImage()
+
 func updateTileImage():
 	tileSprite.set_animation(str(_farmType))
 
@@ -19,6 +26,7 @@ func updateTileImage():
 func _ready():
 	tileSprite.play()
 	hoverSprite.hide()
+	selectedSprite.hide()
 	updateTileImage()
 
 
@@ -26,9 +34,9 @@ func _ready():
 func _process(delta):
 	if hovered:
 		if Input.is_action_just_released('RightClick'):
-			print("right click" + str(_farmType))
+			emit_signal('right_click')
 		elif Input.is_action_just_released('LeftClick'):
-			print("left click" + str(_farmType))
+			emit_signal('left_click')
 
 
 func _on_mouse_entered():
@@ -40,3 +48,9 @@ func _on_mouse_entered():
 func _on_mouse_exited():
 	hovered = false
 	hoverSprite.hide()
+	
+func selected(boo):
+	if boo:
+		selectedSprite.show()
+	else:
+		selectedSprite.hide()
