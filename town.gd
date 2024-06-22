d69a9405ae8f332e3e28f8765aa752aa0eb675e5
Annikatt <anneke256@gmail.com>
2024-06-22 01:11:14 -0400

    to town animation

--- town.gd
+++ town.gd
@@ -8,6 +8,7 @@ signal to_town
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	arrow.play("default")
+	$animation.hide()
 
 
 func _on_mouse_entered():
@@ -22,5 +23,14 @@ func _on_mouse_exited():
 func _unhandled_input(event):
 	if !Global.menu_mode:
 		if hovered and Input.is_action_just_pressed('Activated') and Global.actionPoints > 0:
-			emit_signal('to_town')
-			hide()
+			Global.menu_mode = true
+			$animation.show()
+			$animation.play()
+
+
+func _on_animation_animation_looped():
+	$animation.stop()
+	Global.menu_mode = false
+	emit_signal("to_town")
+	$animation.hide()
+	hide()
